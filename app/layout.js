a4fd9e2c956084d5511744b2a747835f1149d70b
SalDev62 <saloninfo2023@gmail.com>
2025-04-25 16:12:48 +0200

    icon

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <Head>
-        <link rel="icon" href="https://salon-bureau.fr/logo.jpg" sizes="any" />
+        <link rel="icon" href="https://salon-bureau.fr/logo.ico" sizes="any" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
